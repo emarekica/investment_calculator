c08307fc30c063ba82b28509d6dad4f6165e443b
emarekica <emarekica@gmail.com>
2026-02-04 00:01:15 +0100

    Init tests for Investment calculator

---
+++ src/components/Results/Results.test.jsx
@@ -0,0 +1,28 @@
+import { render, screen } from "testing-library/react";
+import Results from "./Results";
+
+describe("Results component", () => {
+  test("Description", () => {
+    render(<Results />);
+
+    const resultsElement =
+  });
+
+
+// renders a table
+// renders correct column headers
+// renders one row per result year
+
+// calculates total interest correctly for first year
+// calculates total interest correctly for later years
+// calculates invested capital correctly
+
+// Assert that formatted strings are rendered
+});
+
+// Arrange
+// Act
+// Assert
+
+    // const headerElement = screen.getByText("React investment calculator", { exact: false });
+    // expect(headerElement).toBeInTheDocument();
---
+++ src/util/investment.test.js
@@ -0,0 +1,5 @@
+// correct number of years returned
+// correct interest calculation
+// correct accumulation over time
+// zero / edge cases
+// immutability (no mutation of inputs)
